8919522ac90114f2be5d6a91391cccb563ce8917
Long Do <thanhlongg8@gmail.com>
2020-09-03 16:01:42 +0700

    create admin_app

---
+++ eShopSolution.AdminApp/Service/IUserApiClient.cs
@@ -0,0 +1,10 @@
+﻿using eShopSolution.ViewModels.System.Users;
+using System.Threading.Tasks;
+
+namespace eShopSolution.AdminApp.Service
+{
+    public interface IUserApiClient
+    {
+        Task<string> Authenticate(LoginRequest request);
+    }
+}
--- eShopSolution.BackendApi/Controllers/UsersController.cs
+++ eShopSolution.BackendApi/Controllers/UsersController.cs
@@ -18,9 +18,9 @@ namespace eShopSolution.BackendApi.Controllers
             _userService = userService;
         }
 
-        [HttpPost("authemticate")]
+        [HttpPost("authenticate")]
         [AllowAnonymous]
-        public async Task<IActionResult> Authenticate([FromForm] LoginRequest request)
+        public async Task<IActionResult> Authenticate([FromBody] LoginRequest request)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
@@ -35,7 +35,7 @@ namespace eShopSolution.BackendApi.Controllers
 
         [HttpPost("register")]
         [AllowAnonymous]
-        public async Task<IActionResult> Register([FromForm] RegisterRequest request)
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
